4a5c1af8c5b8f7f7a5ee40b6a4df1027bbee3ff0
qianxiaoming <kyleqian@gmail.com>
2025-08-23 23:11:49 +0800

    check users data file at startup

--- src/xtyping.rs
+++ src/xtyping.rs
@@ -1,11 +1,28 @@
 #![doc = include_str!("../README.md")]
+
+mod menu;
+mod game;
+
 use bevy::{prelude::*, dev_tools::states::*};
+use bevy::window::{WindowMode, WindowPlugin};
+use serde::Deserialize;
+
+const GAME_APP_TITLE: &str = "超级打字练习";
+const PLAYERS_DATA_FILE: &str = "players.dat";
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: GAME_APP_TITLE.to_string(),
+                mode: WindowMode::Windowed,
+                ..default()
+            }),
+            ..default()
+        }))
         .init_state::<GameState>()
         .add_systems(Startup, setup_game)
+        .add_plugins((menu::menu_plugin, game::game_plugin))
         .run();
 }
 
@@ -20,6 +37,30 @@ enum GameState {
     ConfirmExit
 }
 
-fn setup_game(mut commands: Commands) {
+#[derive(Deserialize, Default)]
+struct Player {
+    name: String,
+    avatar: String,
+    score: u32,
+    level: u32
+}
+
+#[derive(Deserialize, Resource, Default)]
+struct Players(Vec<Player>);
+
+fn setup_game(mut commands: Commands, mut players: ResMut<Players>, mut windows: Query<&mut Window>) {
+    if let Ok(mut window) = windows.single_mut() {
+        window.set_maximized(true);
+    }
+
     commands.spawn(Camera2d);
+
+    if std::path::Path::new(PLAYERS_DATA_FILE).exists() {
+        std::fs::read_to_string(PLAYERS_DATA_FILE)
+            .and_then(|data| serde_json::from_str::<Vec<Player>>(&data).map_err(|err| err.into()))
+            .map(|data| players.0.extend(data))
+            .unwrap_or_else(|err| error!("Failed to parse player data: {}", err));
+    } else {
+        info!("No player data file found in current directory");
+    }
 }
